1f96de803d4bd0be3b31bfbaee2a69bb8f2e234d
PavanGawande <54816127+PavanGawande@users.noreply.github.com>
2019-09-15 14:38:32 +0530

    commented class

--- Udemi/src/main/java/com/onlinetutorial/udemi/commons/CurrentUser.java
+++ Udemi/src/main/java/com/onlinetutorial/udemi/commons/CurrentUser.java
@@ -1,22 +1,19 @@
-package com.onlinetutorial.udemi.commons;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import com.onlinetutorial.udemi.model.User;
-
-@Component
-public class CurrentUser {
-	
-	@Autowired
-	private static LoggingUser loggingUser;
-
-	public static  User getLoggingUser() {
-		return loggingUser.getCurrentUser();
-	}
-
-	public void setLoggingUser(LoggingUser loggingUser) {
-		this.loggingUser = loggingUser;
-	}
-
-}
+/*
+ * package com.onlinetutorial.udemi.commons;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.stereotype.Component;
+ * 
+ * import com.onlinetutorial.udemi.model.User;
+ * 
+ * @Component public class CurrentUser {
+ * 
+ * @Autowired private static LoggingUser loggingUser;
+ * 
+ * public static User getLoggingUser() { return loggingUser.getCurrentUser(); }
+ * 
+ * public void setLoggingUser(LoggingUser loggingUser) { this.loggingUser =
+ * loggingUser; }
+ * 
+ * }
+ */
--- Udemi/src/main/java/com/onlinetutorial/udemi/commons/LoggingUser.java
+++ Udemi/src/main/java/com/onlinetutorial/udemi/commons/LoggingUser.java
@@ -1,37 +1,30 @@
-package com.onlinetutorial.udemi.commons;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.annotation.SessionScope;
-
-import com.onlinetutorial.udemi.dao.UserDao;
-import com.onlinetutorial.udemi.model.User;
-
-@SessionScope
-@Component
-public class LoggingUser {
-	@Autowired
-	private UserDao userDao;
-	private User user;
-
-	public User getUser() {
-		return getCurrentUser();
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public User getCurrentUser() {
-		if (user == null) {
-			UserDetails userdetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication()
-					.getPrincipal();
-
-			user = userDao.findByEmail(userdetails.getUsername());
-		}
-		return user;
-	}
-
-}
+/*
+ * package com.onlinetutorial.udemi.commons;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.security.core.context.SecurityContextHolder; import
+ * org.springframework.security.core.userdetails.UserDetails; import
+ * org.springframework.stereotype.Component; import
+ * org.springframework.web.context.annotation.SessionScope;
+ * 
+ * import com.onlinetutorial.udemi.dao.UserDao; import
+ * com.onlinetutorial.udemi.model.User;
+ * 
+ * @SessionScope
+ * 
+ * @Component public class LoggingUser {
+ * 
+ * @Autowired private UserDao userDao; private User user;
+ * 
+ * public User getUser() { return getCurrentUser(); }
+ * 
+ * public void setUser(User user) { this.user = user; }
+ * 
+ * public User getCurrentUser() { if (user == null) { UserDetails userdetails =
+ * (UserDetails) SecurityContextHolder.getContext().getAuthentication()
+ * .getPrincipal();
+ * 
+ * user = userDao.findByEmail(userdetails.getUsername()); } return user; }
+ * 
+ * }
+ */
